18c750f71a9ad103f6f0537dee852f74dfd89375
Codiving <codivingmoon@gmail.com>
2025-10-13 18:59:46 +0900

    components 배럴 파일 추가

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as CollapsibleNav } from "./CollapsibleNav";
+export { default as Header } from "./Header";
+export { default as MainContents } from "./MainContents";
+export { default as Sidebar } from "./Sidebar";
