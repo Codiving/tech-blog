fe3b8b61db2caf99fb6557dc3dd5bd5705715371
Codiving <codivingmoon@gmail.com>
2025-10-13 18:59:36 +0900

    Header 컴포넌트 css 업데이트

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="sticky top-0 z-10 flex h-16 shrink-0 items-center border-b border-b-gray-300 bg-white px-4">
+    <header className="sticky top-0 z-10 flex h-16 shrink-0 items-center border-b border-b-gray-300 bg-white px-6">
       <Link href="/">
         <div className="flex items-center gap-2">
           <span className="h-10 w-10 rounded-xl bg-black text-center text-xl leading-10 text-white">
