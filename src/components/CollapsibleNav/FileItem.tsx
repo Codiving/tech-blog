83d7a34b4f2e6a1098f7542f696ee30b956fc9db
Codiving <codivingmoon@gmail.com>
2025-10-15 19:32:10 +0900

    마크업 수정

--- src/components/CollapsibleNav/FileItem.tsx
+++ src/components/CollapsibleNav/FileItem.tsx
@@ -7,15 +7,14 @@ interface FileItemProps {
 
 export default function FileItem({ depth, item }: FileItemProps) {
   return (
-    <button
-      className={
-        "w-full cursor-pointer py-0 text-left text-[14px] hover:text-[#333] hover:underline hover:underline-offset-2"
-      }
+    <Link
+      href={`/${item.path ?? ""}`}
+      className="block w-full cursor-pointer py-0 text-left text-[14px] hover:text-[#333] hover:underline hover:underline-offset-2"
       style={{
         paddingLeft: (depth - 1) * 12,
       }}
     >
-      <Link href={item.path ?? ""}> {item.label}</Link>
-    </button>
+      {item.label}
+    </Link>
   );
 }
